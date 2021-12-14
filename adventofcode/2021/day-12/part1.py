249e16c3a4883e433418ead0502a4dd9e771a164
valgrut <naruto987@seznam.cz>
2021-12-14 01:40:28 +0100

    Removed comments

--- adventofcode/2021/day-12/part1.py
+++ adventofcode/2021/day-12/part1.py
@@ -1,13 +1,7 @@
 #! /usr/sbin/env python3
 
-fdata = open("input_19.txt", 'r')
 
 def recursive(level, node, current_closed):
-    # print()
-    # print(level, "Recursion for ", node)
-    # print(level, "current stack: ", rstack)
-    # print(level, "current closed: ", current_closed)
-    
     # init values for this recursion level
     closed = current_closed.copy()
     rstack.append(node)
@@ -15,26 +9,22 @@ def recursive(level, node, current_closed):
     # if currently pushed node is "end", print and pop()
     if node == "end":
         print(level, ">> Found path", rstack)
-        # print(level, "Returning.")
         rstack.pop()
         return
 
     # do closed pouze pokud to je 'lowercase'
     if top.islower():
-        # print(level, " ", top, "appended to closed")
         closed.append(top)
     
-    # print("where to go from (",top,"):", graph[top], " | ", closed)
     for next in graph[top]:
         # if top is not on closed yet
-        # print(level, "Next in graph is ", next, ". Is in closed?", next in closed)
         if next not in closed:
-            # print(level, "Going from ", top, "to ", next)
             recursive(level+1, next, closed)
-            # print(level, "Back from ", next, " in ", top)
     # maybe here remove this node from stack (TOP)
     rstack.pop()
 
+
+fdata = open("input.txt", 'r')
 
 # Initiate graph nodes and connect them in both directions
 graph = {}
@@ -47,9 +37,9 @@ for line in fdata:
     graph[conn[0]].append(conn[1])
     graph[conn[1]].append(conn[0])
  
+
 outer_closed = []
 rstack = []
-
 recursive(1, "start", outer_closed.copy())
 
 
