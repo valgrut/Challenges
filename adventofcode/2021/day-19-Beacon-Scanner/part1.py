24af52fe930d782fbfc030adb2a711f27a05dae0
Jiri Peska <jpeska@redhat.com>
2023-03-08 12:57:06 +0100

    Progress, but still does not count all beacons

--- adventofcode/2021/day-19-Beacon-Scanner/part1.py
+++ adventofcode/2021/day-19-Beacon-Scanner/part1.py
@@ -53,35 +53,41 @@ def have_12_common(list1, list2):
     """
     # Find the 12 same distances between points of two arrays
     distances = {}
-    final = []
+    distances_points = {}
     for cidx1 in range(len(list1)):
         for cidx2 in range(len(list2)):
-
             distance = str(round_down(math.dist(list1[cidx1], list2[cidx2]), 3))
             if distance in distances.keys():
                 distances[distance] += 1
-                final.append(list1[cidx1])
+                # print(list1[cidx1])
+                # print(distances_points[distance])
+                # if any(list1[cidx1] == point for point in distances_points[distance]):
+                # print(list1[cidx1].tolist())
+                # print(list1[cidx1])
+                # print()
+                # print(distances_points[distance])
+                if list1[cidx1].tolist() not in distances_points[distance]:
+                    distances_points[distance].append(list1[cidx1].tolist())
             else:
                 distances[distance] = 1
+                distances_points[distance] = []
+                distances_points[distance].append(list1[cidx1].tolist())
 
-    # min_common_points = 12  # Default: 12
-    # if any(dist_num >= min_common_points for dist_num in distances.values()):
-        # print(distances)
     for dist, dist_num in distances.items():
         if dist_num >= 12:
-            print(dist, dist_num)
-            # print(distances)
-            print(final)
-            return True
+            # print(dist, dist_num)
+            # print(distances_points[dist])
+            # return True
+            return distances_points[dist]
 
-    return False
-
+    return []
 
 def find_corelation(ref_list, checked_list):
     """
     Transform input list of points to all 24 possible positions and
     return True, if some of the rotations have common points with second list of points.
     """
+    common_beacons = []
     operations = []
     for flip in range(1, 5):
         operations.append("flip90up")
@@ -89,9 +95,11 @@ def find_corelation(ref_list, checked_list):
         for rot in range(1, 5):
             operations.append("rot90left")
             rot90left(checked_list)
-            if have_12_common(ref_list, checked_list):
+            common_points = have_12_common(ref_list, checked_list)
+            if common_points:
                 print(operations)
-                return True
+                # return True
+                return common_points
 
     for flip in range(1, 5):
         operations.append("flip90left")
@@ -99,11 +107,14 @@ def find_corelation(ref_list, checked_list):
         for rot in range(1, 5):
             operations.append("rot90left")
             rot90left(checked_list)
-            if have_12_common(ref_list, checked_list):
+            common_points = have_12_common(ref_list, checked_list)
+            if common_points:
                 print(operations)
-                return True
+                # return True
+                return common_points
 
-    return False
+    return []
+    # return False
 
 # ===============================================================================================
 
@@ -112,9 +123,10 @@ data_file = "input_t2.txt"  # Test data - 3D - arrangement of beacons as seen fr
 data_file = "input_t2_sub.txt"  # Test data - 3D - subset of above
 data_file = "input_t2_sub2.txt"  # Test data - 3D - subset of above
 # data_file = "input_t4_crafted.txt"  # Test data - 3D - subset of above
-# data_file = "input_t3.txt"  # Test data - 3D - larger test input
-data_file = "input_t3_sub1.txt"  # Test data - 3D - subset
-# data_file = "input1.txt"  # Part 1 input data
+data_file = "input_t3.txt"  # Test data - 3D - larger test input
+# data_file = "input_t3_sub1.txt"  # Test data - 3D - subset
+# data_file = "i1.txt"  # Test data - 3D - subset
+data_file = "input1.txt"  # Part 1 input data
 # data_file = "input2.txt"  # Part 2 input data
 
 fdata = open(data_file, 'r')
@@ -123,8 +135,6 @@ fdata = open(data_file, 'r')
 scanner_regex = re.compile(r'--- scanner [0-9]{1,3} ---')
 scanner_reports = {}
 scanner_count = -1
-
-numpy_reports = {}
 
 # ==== Loading of input data
 for line in fdata:
@@ -141,17 +151,40 @@ for line in fdata:
 
     scanner_reports[scanner_count].append([int(x) for x in line.split(',')])
 
-
 # ==== Data procession
 # plot_points(scanner_reports)
 
-print(scanner_reports[0])
-arr0 = np.array(scanner_reports[0]) # + 0.1
-arr1 = np.array(scanner_reports[1])
+beacons = []
+# print(scanner_reports[1])
+for scanner_x, report_x in enumerate(list(scanner_reports.values())):
+    # print(scanner_reports[scanner_x])
+    for scanner_y, report_y in enumerate(list(scanner_reports.values())[:scanner_x]):
+        print("Porovnavam:", scanner_x, scanner_y)
+        arr0 = np.array(report_x)
+        arr1 = np.array(report_y)
+        common_beacons = find_corelation(arr0, arr1)
+        if not common_beacons:
+            print("Could not find corelation")
+        else:
+            # if common_beacons not in beacons:
+            beacons += common_beacons
 
-if not find_corelation(arr0, arr1):
-    print("Could not find corelation")
+# lst = beacons
+# # 1. Convert into list of tuples
+# tpls = [tuple(x) for x in lst]
+# # 2. Create dictionary with empty values and
+# # 3. convert back to a list (dups removed)
+# dct = list(dict.fromkeys(tpls))
+# # 4. Convert list of tuples to list of lists
+# dup_free = [list(x) for x in dct]
+# # Print everything
+# print("deduplikovane beacons", len(dup_free))
+# print(dup_free)
 
-plot_points({0: arr0, 1: arr1})
+# print("Pocet nalezenych beacons", len(list( dict.fromkeys(beacons) )))
+print("Pocet nalezenych beacons: ", len(beacons))
+# print(list( dict.fromkeys(beacons) ))
+print(beacons)
+# plot_points({0: arr0, 1: arr1})
 
 
